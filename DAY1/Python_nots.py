fb5c7a9ec5855182dfff0e0e7c8342962f754790
yavraj <yuvrajmaurya85420@gmail.com>
2026-08-19 09:20:28 +0530

    variables

---
+++ DAY1/Python_nots.py
@@ -0,0 +1,43 @@
+# What is a programming language?
+
+# A programming language is a formal language comprising a set of instructions 
+# that can be used to produce various kinds of output.
+# Programming languages are used in computer programming to implement algorithms and manipulate data structures.
+# They provide a way for humans to communicate with computers and create software applications, scripts, 
+# or other sets of instructions that a computer can execute.
+
+# Machine code is the lowest-level programming language, consisting of binary code that the computer's hardware can directly execute.
+
+# Machine code is specific to a particular computer architecture and is not easily readable or writable by humans.
+
+
+
+# Machine  =====================> (compiler or interpreter) =====================>  High-level programming language(coding)
+
+
+
+# what is a high-level programming language? 
+
+
+
+# A high-level programming language is a programming language that is designed to be easy for
+#  humans to read and write, and that abstracts away the complexities of the underlying hardware.
+
+
+
+# what is python?
+
+
+
+# Python is a high-level programming language that is known for its simplicity and readability. 
+# It is an interpreted language, which means that the code is executed directly without needing to be
+#  compiled first. Python is widely used in various fields such as web development, data science, 
+# artificial intelligence, and automation.
+
+
+
+
+# file name in python is case sensitive.
+# python character set is case sensitive. For example,
+# the variable name "myVariable" is different from "myvariable" or "MYVARIABLE".
+
