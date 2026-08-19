41ffbd24a450a50136cfa8fed53444146a63e46a
yavraj <yuvrajmaurya85420@gmail.com>
2026-08-19 09:49:43 +0530

    Data_types

---
+++ DAY2/Data_types.py
@@ -0,0 +1,100 @@
+# data type in python is a classification that specifies which type of 
+# value a variable can hold. Python has several built-in data types, including:
+
+
+# Data types (int, float, str, bool)
+
+# 1. Numeric types: int, float, complex
+# 2. Sequence types: list, tuple, range
+# 3. Mapping types: dict
+# 4. Set types: set, frozenset
+# 5. Boolean type: bool
+# 6. None type: NoneType
+# 7. String type: str
+
+#Example of data types in Python:
+
+# Integer data type
+x = 10
+print(x)
+
+#print the data type of x
+print(type(x))
+
+
+
+# note: In Python, you can use the type() function to check the data type of a variable.
+
+
+
+# Float data type
+y = 3.14
+print(y)
+
+#print the data type of y
+print(type(y)) 
+
+# String data type
+name = "Alice"
+print(name)
+
+#print the data type of name
+print(type(name))
+
+# Boolean data type
+is_student = True
+print(is_student)
+
+#print the data type of is_student
+print(type(is_student)) 
+
+#null data type
+z = None
+print(z)
+
+#print the data type of z
+print(type(z)) 
+
+#number data type
+a = 5
+print(a)
+
+#print the data type of a
+print(type(a))
+
+
+#set data type
+my_set = {1, 2, 3}
+print(my_set)
+
+#print the data type of my_set
+print(type(my_set))
+
+#tuple data type
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+#print the data type of my_tuple
+print(type(my_tuple))
+
+
+#list data type
+my_list = [1, 2, 3]
+print(my_list)
+
+#print the data type of my_list
+print(type(my_list))
+
+#dictionary data type
+my_dict = {"name": "Alice", "age": 25}
+print(my_dict)
+
+#print the data type of my_dict
+print(type(my_dict))
+
+# keywords in python are reserved words that have a special meaning 
+# and cannot be used as identifiers (variable names, function names, etc.).
+
+# ( and , else, for, while, if, elif, else, def, return, import,
+#   from, as, class, try, except, finally, with, lambda, pass, break, continue, global,
+#     nonlocal, assert, yield, raise, del , print, input, True, False, None, and, or, not, is, in, as, async, await , )
