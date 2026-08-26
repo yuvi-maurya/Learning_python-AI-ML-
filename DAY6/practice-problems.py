51de138be2aa9bafebb771828acc040bb38670cc
yavraj <yuvrajmaurya85420@gmail.com>
2026-08-26 16:37:42 +0530

    Solved 30 beginner Python problems today, including the classic FizzBuzz. Practice > just watching tutorials.

--- DAY6/practice-problems.py
+++ DAY6/practice-problems.py
@@ -1,356 +1,356 @@
-# # Question: Do numbers input lo aur unka sum, difference, product, division print karo.
+# Question: Do numbers input lo aur unka sum, difference, product, division print karo.
 
-# num1 = int(input("Enter your number:"))
-# num2 = int(input("Enter your number:"))
+num1 = int(input("Enter your number:"))
+num2 = int(input("Enter your number:"))
 
-# sum = num1 + num2
+sum = num1 + num2
 
-# diff = num1 - num2
+diff = num1 - num2
 
-# product_result = num1 * num2
+product_result = num1 * num2
 
-# if num2 != 0:
-#     division_result = num1 / num2
-# else:
-#     print("Division by zero not possible")
+if num2 != 0:
+    division_result = num1 / num2
+else:
+    print("Division by zero not possible")
 
 
-# print(sum)
-# print(diff)
-# print(product_result)
-# print(division_result)
+print(sum)
+print(diff)
+print(product_result)
+print(division_result)
 
 
-# #Question: Ek number ka square aur cube nikalo.
+#Question: Ek number ka square aur cube nikalo.
 
-# num1 = int(input("Enter your number:"))
+num1 = int(input("Enter your number:"))
 
-# square = num1 ** 2
+square = num1 ** 2
 
-# cube = num1 ** 3
+cube = num1 ** 3
 
-# print(square)
-# print(cube)
+print(square)
+print(cube)
 
-# # Question: Temperature ko Celsius se Fahrenheit mein convert karo.
+# Question: Temperature ko Celsius se Fahrenheit mein convert karo.
 
-# Celsius = int(input("Enter temperature in Celsius:"))
+Celsius = int(input("Enter temperature in Celsius:"))
 
-# fahrenheit = (Celsius * 9/5) + 32
+fahrenheit = (Celsius * 9/5) + 32
 
-# print(fahrenheit)
+print(fahrenheit)
 
 
-# # Question: Simple interest calculate karo (P, R, T input lekar).
+# Question: Simple interest calculate karo (P, R, T input lekar).
 
-# P = float(input("Principal amount enter karo: "))
-# R = float(input("Rate of interest (%) enter karo: "))
-# T = float(input("Time (years) enter karo: "))
+P = float(input("Principal amount enter karo: "))
+R = float(input("Rate of interest (%) enter karo: "))
+T = float(input("Time (years) enter karo: "))
 
-# SI = (P * R * T) / 100
+SI = (P * R * T) / 100
 
-# print(f"Simple interest: {SI}")
+print(f"Simple interest: {SI}")
 
-# # Question: Ek rectangle ka area aur perimeter nikalo (length, breadth input).
+# Question: Ek rectangle ka area aur perimeter nikalo (length, breadth input).
 
 
-# length = float(input("Enter rectangle length:"))
-# breadth = float(input("Enter rectangle breadth:"))
+length = float(input("Enter rectangle length:"))
+breadth = float(input("Enter rectangle breadth:"))
 
-# area = length * breadth
+area = length * breadth
 
-# perimeter = 2 * ( length + breadth)
+perimeter = 2 * ( length + breadth)
 
-# print(f"Area of a rectangle: {area}")
+print(f"Area of a rectangle: {area}")
 
-# print(f"Perimeter of a Rectabgle: {perimeter}")
+print(f"Perimeter of a Rectabgle: {perimeter}")
 
 
 
-# # Question: Do numbers swap karo (bina third variable use kiye).
+# Question: Do numbers swap karo (bina third variable use kiye).
 
-# a = 5 
-# b = 7
+a = 5 
+b = 7
 
-# print(f"Print number A: {a}")
-# print(f"Print number B: {b}")
+print(f"Print number A: {a}")
+print(f"Print number B: {b}")
 
 
-# # tarika 1
+# tarika 1
 
-# a = a + b 
+a = a + b 
 
-# b = a - b
-# a = a - b
+b = a - b
+a = a - b
 
-# print(f"Print number A: {a}")
-# print(f"Print number B: {b}")
+print(f"Print number A: {a}")
+print(f"Print number B: {b}")
 
 
 
-# #   tarika 2
+#   tarika 2
 
 
-# a , b = b , a
+a , b = b , a
 
-# print(f"Print number A: {a}")
-# print(f"Print number B: {b}")
+print(f"Print number A: {a}")
+print(f"Print number B: {b}")
 
 
-# # Question: 
+# Question: 
 
-# # Ek number ki last digit nikalo.
+# Ek number ki last digit nikalo.
 
-# num = int(input("Enter your number:"))
+num = int(input("Enter your number:"))
 
-# last_digit = num % 10
+last_digit = num % 10
 
-# print("Last digit hai:", last_digit)
+print("Last digit hai:", last_digit)
 
 
 
 
-# # Question: Circle ka area nikalo (radius input, π = 3.14159).
+# Question: Circle ka area nikalo (radius input, π = 3.14159).
 
 
 
-# redius = float(input("Enter Redius:"))
+redius = float(input("Enter Redius:"))
 
-# area = (3.14159) * redius ** 2
+area = (3.14159) * redius ** 2
 
-# print(f"Area of Circle: {area}")
+print(f"Area of Circle: {area}")
 
 
-# # Question 9. Number even hai ya odd, check karo.
+# Question 9. Number even hai ya odd, check karo.
 
-# num = int(input("Enter your number:"))
+num = int(input("Enter your number:"))
 
-# if num % 2 == 0:
-#     print(f"Number {num} is even")
-# else:
-#     print(f"Number {num} is odd")
+if num % 2 == 0:
+    print(f"Number {num} is even")
+else:
+    print(f"Number {num} is odd")
 
 
-# # Question 10. Teen numbers mein sabse bada number find karo.
+# Question 10. Teen numbers mein sabse bada number find karo.
 
 
-# num1 = int(input("Enter your number1:"))
-# num2 = int(input("Enter your number2:"))
-# num3 = int(input("Enter your number3:"))
+num1 = int(input("Enter your number1:"))
+num2 = int(input("Enter your number2:"))
+num3 = int(input("Enter your number3:"))
 
-# if num1 > num2 or num1 > num3:
-#     print(f"num1: {num1} is largest number")
-# elif num2 > num3:
-#     print(f"num2: {num2} is largest number")
-# else:
-#     print(f"num3: {num3} is largest number")
+if num1 > num2 or num1 > num3:
+    print(f"num1: {num1} is largest number")
+elif num2 > num3:
+    print(f"num2: {num2} is largest number")
+else:
+    print(f"num3: {num3} is largest number")
 
 
 
 
-# # Question 11. Ek number positive, negative ya zero hai, check karo.
+# Question 11. Ek number positive, negative ya zero hai, check karo.
 
-# num = int(input("Enter your number:"))
+num = int(input("Enter your number:"))
 
-# if num > 0:
-#     print(f"{num} is positive number")
-# elif num < 0:
-#     print(f"{num} is negative number")
-# else:
-#     print(f"Number is zero")
+if num > 0:
+    print(f"{num} is positive number")
+elif num < 0:
+    print(f"{num} is negative number")
+else:
+    print(f"Number is zero")
 
 
-# # Question 12. Student ke marks input lo aur grade decide karo (A/B/C/F).
-# # out of 100
+# Question 12. Student ke marks input lo aur grade decide karo (A/B/C/F).
+# out of 100
 
-# num = int(input("Enter your number:"))
+num = int(input("Enter your number:"))
 
-# if num >= 90:
-#     grade = "A"
-# elif 90 > num >= 80:
-#     grade = "B"
-# elif 80 > num >=70:
-#     grade = "C"
-# else:
-#     grade = "F"
+if num >= 90:
+    grade = "A"
+elif 90 > num >= 80:
+    grade = "B"
+elif 80 > num >=70:
+    grade = "C"
+else:
+    grade = "F"
 
-# print(f" Grade: {grade}")
+print(f" Grade: {grade}")
 
 
 
-# # Question 13. Year leap year hai ya nahi, check karo.
+# Question 13. Year leap year hai ya nahi, check karo.
 
-# year = int(input("Year enter karo: "))
+year = int(input("Year enter karo: "))
 
-# # Leap year check karna
-# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-#     print(year, "leap year hai")
-# else:
-#     print(year, "leap year nahi hai")
+# Leap year check karna
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "leap year hai")
+else:
+    print(year, "leap year nahi hai")
 
 
-# # Question 14. Ek number vowel hai ya consonant (character input lekar).
+# Question 14. Ek number vowel hai ya consonant (character input lekar).
 
-# ch = input("Ek character enter karo: ")
+ch = input("Ek character enter karo: ")
 
-# # Lowercase mein convert kar diya taaki 'A' aur 'a' dono handle ho jaayein
-# ch = ch.lower()
+# Lowercase mein convert kar diya taaki 'A' aur 'a' dono handle ho jaayein
+ch = ch.lower()
 
-# if ch in ['a', 'e', 'i', 'o', 'u']:
-#     print(ch, "ek vowel hai")
-# else:
-#     print(ch, "ek consonant hai")
+if ch in ['a', 'e', 'i', 'o', 'u']:
+    print(ch, "ek vowel hai")
+else:
+    print(ch, "ek consonant hai")
 
 
 
-# # Question 15. Simple calculator banao (+, -, *, / — operator input lekar if-elif se).
+# Question 15. Simple calculator banao (+, -, *, / — operator input lekar if-elif se).
 
-# num1 = float(input("Pehla number enter karo: "))
-# num2 = float(input("Dusra number enter karo: "))
-# op = input("Operator enter karo (+, -, *, /): ")
+num1 = float(input("Pehla number enter karo: "))
+num2 = float(input("Dusra number enter karo: "))
+op = input("Operator enter karo (+, -, *, /): ")
 
-# if op == '+':
-#     result = num1 + num2
-# elif op == '-':
-#     result = num1 - num2
-# elif op == '*':
-#     result = num1 * num2
-# elif op == '/':
-#     if num2 != 0:
-#         result = num1 / num2
-#     else:
-#         result = "Error: Division by zero"
-# else:
-#     result = "Invalid operator"
+if op == '+':
+    result = num1 + num2
+elif op == '-':
+    result = num1 - num2
+elif op == '*':
+    result = num1 * num2
+elif op == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error: Division by zero"
+else:
+    result = "Invalid operator"
 
-# print("Result:", result)
+print("Result:", result)
 
-# # Question 16. Triangle valid hai ya nahi, check karo (3 sides input, triangle inequality).
+# Question 16. Triangle valid hai ya nahi, check karo (3 sides input, triangle inequality).
 
 
-# a = float(input("Side A enter karo: "))
-# b = float(input("Side B enter karo: "))
-# c = float(input("Side C enter karo: "))
+a = float(input("Side A enter karo: "))
+b = float(input("Side B enter karo: "))
+c = float(input("Side C enter karo: "))
 
-# if (a + b > c) and (b + c > a) and (a + c > b):
-#     print("Yeh triangle valid hai")
-# else:
-#     print("Yeh triangle valid nahi hai")
+if (a + b > c) and (b + c > a) and (a + c > b):
+    print("Yeh triangle valid hai")
+else:
+    print("Yeh triangle valid nahi hai")
 
 
-# # Loops — Basic (17-22)
+# Loops — Basic (17-22)
 
 
-# # Question 17. 1 se 10 tak table print karo (kisi bhi number ka).
-# num = int(input("enter any number:"))
-# for i in range(1, 11):
-#     tabel = (f"{num} X {i} = {num * i}")
-#     print(tabel)
+# Question 17. 1 se 10 tak table print karo (kisi bhi number ka).
+num = int(input("enter any number:"))
+for i in range(1, 11):
+    tabel = (f"{num} X {i} = {num * i}")
+    print(tabel)
 
 
-# # Question 18. 1 se N tak sabhi numbers ka sum nikalo.
+# Question 18. 1 se N tak sabhi numbers ka sum nikalo.
 
-# num = int(input ("Enter your number"))
-# sum = 0
-# for i in range(1, num + 1):
-#     sum += i
-# print(sum)
+num = int(input ("Enter your number"))
+sum = 0
+for i in range(1, num + 1):
+    sum += i
+print(sum)
 
 
-# # Question 19. Ek number ka factorial nikalo.
+# Question 19. Ek number ka factorial nikalo.
 
-# num = int(input ("Enter your number"))
-# factorial = 1
-# for i in range(1, num + 1):
-#     factorial*= i
-# print(factorial)
+num = int(input ("Enter your number"))
+factorial = 1
+for i in range(1, num + 1):
+    factorial*= i
+print(factorial)
 
 
-# # Question 20. N tak ke sabhi even numbers print karo.
+# Question 20. N tak ke sabhi even numbers print karo.
 
-# num = int(input ("Enter your number"))
+num = int(input ("Enter your number"))
 
-# for i in range(1, num + 1):
-#     if i % 2 == 0:
-#         print(i)
+for i in range(1, num + 1):
+    if i % 2 == 0:
+        print(i)
 
 
-#         #OR
+        #OR
 
-# for i in range(2, num +1, 2):
-#     print(i)
+for i in range(2, num +1, 2):
+    print(i)
 
 
-# # Question 21. Fibonacci series print karo (N terms tak).
+# Question 21. Fibonacci series print karo (N terms tak).
 
-# # N terms input lena
-# n = int(input("Kitne terms chahiye: "))
+# N terms input lena
+n = int(input("Kitne terms chahiye: "))
 
-# # Pehle 2 numbers
-# first = 0
-# second = 1
+# Pehle 2 numbers
+first = 0
+second = 1
 
-# print("Fibonacci Series:")
+print("Fibonacci Series:")
 
-# if n <= 0:
-#     print("Please enter a positive number")
-# elif n == 1:
-#     print(first)
-# else:
-#     print(first, second, end=" ")
-#     for i in range(2, n):
-#         next_num = first + second
-#         print(next_num, end=" ")
-#         first = second
-#         second = next_num
+if n <= 0:
+    print("Please enter a positive number")
+elif n == 1:
+    print(first)
+else:
+    print(first, second, end=" ")
+    for i in range(2, n):
+        next_num = first + second
+        print(next_num, end=" ")
+        first = second
+        second = next_num
 
 
 
-# # Question 22. Number palindrome hai ya nahi, check karo (e.g. 121).
+# Question 22. Number palindrome hai ya nahi, check karo (e.g. 121).
 
 
-# num = int(input("Enter your number"))
-# original = num
-# new_num = 0
-# while num > 0:
-#     last_digit = num % 10
-#     new_num = new_num * 10 + last_digit
-#     num //= 10
+num = int(input("Enter your number"))
+original = num
+new_num = 0
+while num > 0:
+    last_digit = num % 10
+    new_num = new_num * 10 + last_digit
+    num //= 10
 
-# if original == new_num:
-#     print(f"{new_num} is palindrome")
-# else:
-#     print(f"{new_num} is not palindrome")
+if original == new_num:
+    print(f"{new_num} is palindrome")
+else:
+    print(f"{new_num} is not palindrome")
 
 
 
 
-# # Question 23. Classic FizzBuzz: 1-100 tak, 3 ke multiple pe "Fizz", 5 ke multiple pe "Buzz", dono pe "FizzBuzz".
+# Question 23. Classic FizzBuzz: 1-100 tak, 3 ke multiple pe "Fizz", 5 ke multiple pe "Buzz", dono pe "FizzBuzz".
 
 
-# for i in range(1, 101):
-#     if i % 3 == 0 and i % 5 == 0:
-#         print("FizzBuzz", end=" ")
-#     elif i % 3 == 0:
-#         print("Fizz", end=" ")
-#     elif i % 5 == 0:
-#         print("Buzz", end=" ")
-#     else:
-#         print(i, end=" ")
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz", end=" ")
+    elif i % 3 == 0:
+        print("Fizz", end=" ")
+    elif i % 5 == 0:
+        print("Buzz", end=" ")
+    else:
+        print(i, end=" ")
 
 
-# # Question 24. FizzBuzz variant: 3 ke multiple pe "Fizz", 7 ke multiple pe "Buzz" (different numbers try karo).
+# Question 24. FizzBuzz variant: 3 ke multiple pe "Fizz", 7 ke multiple pe "Buzz" (different numbers try karo).
 
 
-# for i in range(1, 101):
-#     if i % 3 == 0 and i % 7 == 0:
-#         print("FizzBuzz", end=" ")
-#     elif i % 3 == 0:
-#         print("Fizz", end=" ")
-#     elif i % 7 == 0:
-#         print("Buzz", end=" ")
-#     else:
-#         print(i, end=" ")
+for i in range(1, 101):
+    if i % 3 == 0 and i % 7 == 0:
+        print("FizzBuzz", end=" ")
+    elif i % 3 == 0:
+        print("Fizz", end=" ")
+    elif i % 7 == 0:
+        print("Buzz", end=" ")
+    else:
+        print(i, end=" ")
 
 
 
@@ -373,3 +373,7 @@ for num in range(1, 51):
         print(num, "- Prime")
     else:
         print(num)        
+
+
+
+
